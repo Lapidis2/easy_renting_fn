d036072554b926754477d80d2655711cbe854878
habyarimana caleb <callebhabyar55@gmail.com>
2025-04-24 02:17:40 +0200

    add role in register

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -9,6 +9,7 @@ const Register = () => {
   const [formData, setFormData] = useState({
     username: '',
     email: '',
+    role: '',
     password: '',
     confirmPassword: '',
   });
@@ -87,6 +88,20 @@ const Register = () => {
               value={formData.email} 
               onChange={handleChange}
               placeholder="Enter your Email"
+              className="mt-1 block w-full px-3 py-2 border rounded-md shadow-sm text-sm focus:ring-blue-500 focus:border-blue-500"
+            />
+          </div>
+          <div className="mb-4">
+            <label htmlFor="role" className="block text-sm font-medium text-gray-700">
+              Role
+            </label>
+            <input
+              type="text"
+              id="role"
+              name="role"
+              value={formData.role} 
+              onChange={handleChange}
+              placeholder="Your Role; e.g. buyer, seller, guest"
               className="mt-1 block w-full px-3 py-2 border rounded-md shadow-sm text-sm focus:ring-blue-500 focus:border-blue-500"
             />
           </div>
